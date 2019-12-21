5835c97e3b42f44c507b37d171a94aed17f2c1ed
Dessy Owiti <owitidessy@yahoo.com>
2019-12-21 02:00:10 +0000

    Setup Single Table Inheritance (STI) on the Pieces. The pieces pawn, knight, bishop, rook, queen and king inherit functionality from chessPiece model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_16_094223) do
+ActiveRecord::Schema.define(version: 2019_12_19_111208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chess_pieces", force: :cascade do |t|
+    t.integer "piece_id"
+    t.integer "game_id"
+    t.string "color"
+    t.string "type"
+    t.integer "position_x"
+    t.integer "position_y"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
