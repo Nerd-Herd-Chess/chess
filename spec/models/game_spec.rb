b79b0d527a452cb2042f1bd91e05ee182edd1f91
Olivia Goehrig <goehrigo@gmail.com>
2020-01-17 03:43:42 +0000

    olivia's changes to is_obstructed and unit testing

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  let!(:game) { FactoryBot.create(:game) }
-  let!(:chess_piece) { FactoryBot.create(:chess_piece, position_x: 1, position_y: 1, game_id: game.id)}
- 
-  describe 'space_occupied?' do
+let!(:game) { FactoryBot.create(:game) }
+let!(:chess_piece) { FactoryBot.create(:chess_piece, position_x: 1, position_y: 1, game_id: game.id)}
+
+  describe 'space_occupied?' do  
     it 'returns true if there is a piece on the provided space' do
       expect(game.space_occupied?(1, 1)).to be true
     end
@@ -12,10 +12,11 @@ RSpec.describe Game, type: :model do
     it 'returns true if there is a piece on the provided space' do
       expect(game.space_occupied?(1, 2)).to be false
     end
+
+    it 'return true if there is a piece on the provided space' do
+      game1 = FactoryBot.create(:game)
+      game1.fill_for_testing
+      expect(game1.space_occupied?(0, 0)).to be true
+    end
   end
 end
-
-
-#is_obstructed? A6 -> C4 => false
-#is_obstructed? F1 -> D3 => true
-#is_obstructed? A1 -> A4 => true
