27401c8b00d5d88f0662f9122373e65129b70220
gnvvbll <gnvvbll@gmail.com>
2020-01-20 20:31:45 -0500

    submission page for starting a game

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,3 +1,11 @@
 class Bishop < ChessPiece 
-  
+    def valid_move?(end_x, end_y)
+        delta_x = (position_x - end_x).abs
+        delta_y = (position_y - end_y).abs
+        
+        delta_y / delta_x == 1
+      end
+    
+    
+    end
 end
