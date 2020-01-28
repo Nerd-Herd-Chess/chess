2e36b92b6a2b93df4fe750a727f67d0ef2687b3c
Dessy Owiti <owitidessy@yahoo.com>
2020-01-28 03:48:56 +0000

    Completed queen.rb and queen_spec.rb files. All tests passed

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -2,7 +2,10 @@ class Queen < ChessPiece
   def valid_move?(end_x, end_y)
     delta_x = (position_x - end_x).abs # position_x same as start_x. abs means absolute value so when we get the difference answer will always be a positive value
     delta_y = (position_y - end_y).abs
-    slope = (end_y - position_y).to_i / (end_x - position_x).to_i
+    if delta_x==0
+      return true 
+    end  
+    slope = (delta_y).to_i / (delta_x).to_i
     
     #delta_x < 9 && delta_y < 9 && ('diagonal' || 'horizontal' || 'vertical')
     delta_x < 9 && delta_y < 9 && (slope.abs == 1.0 || position_y == end_y || position_x == end_x)
